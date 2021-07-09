22ab2656fee850ee114d0ddaefbe4c113742a507
Leslie Dobrowsky <Leslie.Dobrowsky@stats.govt.nz>
2021-07-09 13:50:59 +1200

    product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useEffect, useState } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
+
 function App() {
+  const [products, setProducts] = useState();
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
+
+  useEffect(() => {
+    setProducts(data.products);
+  }, []);
+
+  //console.log("console :", products);
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
       <footer>All rights reserved.</footer>
     </div>
   );
